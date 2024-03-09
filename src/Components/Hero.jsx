ed33a832b1ca06260abbd2dc058438c7dda03cfb
Huseyin-Celebi <hcelebi057@gmail.com>
2024-03-09 22:08:14 +0300

    ilk bölüm sonu

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,25 +5,22 @@ import DarkMode from '../Layout/DarkMode.jsx'
 
 
 function Hero(props) {
-  const Mode = localStorage.getItem("theme")
-  console.log(Mode)
+  console.log("mode", localStorage.theme) 
   return (
   
     <main className='flex w-full h-[671px]'>
       <div className='bg-mv w-[70%] h-[671px] dark:bg-darkMv'>
-        <div className='flex relative w-[960px] h-[72px] top-[37px] left-[200px]'> 
+        <div className='flex relative w-[960px] h-[72px] top-[37px] left-[152px]'> 
           <p className='font-int text-[32px] font-bold text-[32px] leading-[72px] text-left  text-fy'>almila</p>
           <a className='relative flex text-[#777777] hover:text-slate-100 
-          w-[138px] h-[18px] left-[450px]  text-sm font-ınt font-bold' href="/"><p className='text-fy dark:text-[#BAB2E7]'>TÜRKÇE</p>'YE GEÇ</a>
+          w-[138px] h-[18px] left-[524px]  text-sm font-ınt font-bold' href="/"><p className='text-fy dark:text-[#BAB2E7]'>TÜRKÇE</p>'YE GEÇ</a>
         </div>
         <HeroContent />
       </div>
       <div className='grid flex w-[532px] h-[671px] bg-fy dark:bg-darkFy'>
         <div className='relative flex w-[175px] h-[24px] gap-[16px] top-[35px] left-[10px]'>
           <DarkMode />
-          <p className='text-mv font-int text-[15px] font-bold'>
-            {Mode ? "LIGTH MODE" : "DARK MODE"}
-          </p>
+          
         </div>
       </div>
     </main>  
@@ -32,16 +29,5 @@ function Hero(props) {
 
 export default Hero
 
-/* 
-#4731D3
-font-family: Inter;
-font-size: ;
-font-weight: 700;
-line-height: 18px;
-letter-spacing: 0.1em;
-text-align: left;
-width: 103px
-height: 18px
-top: 3px
-left: 64px */
+
 
--- src/Layout/DarkMode.jsx
+++ src/Layout/DarkMode.jsx
@@ -6,8 +6,10 @@ import './toggle.css';
 function DarkMode() {
   const [toggled, setToggled] = useState(false);
   const element = document.documentElement
-  console.log(localStorage.theme)
-  
+
+  function onWindowMatch(){
+
+  }
 
   useEffect(() => {
     switch (toggled) {
@@ -29,15 +31,20 @@ function DarkMode() {
     setToggled(!toggled)
   }
   return (
-    <div className="App">
+    <div className="App flex gap-[10px]">
       <button
-        className={`toggle-btn ${toggled ? "toggled" : ""} dark:bg-[#3A3A3A]`}
+        className={`toggle-btn ${toggled ? "toggled" : ""} dark:bg-[#3A3A3A] top-[0px]`}
         onClick={handleClick}
       >
         <div className="thumb"></div>
         <div className="thumb2 bg-[#8F88FF] dark:bg-[#3A3A3A]"></div>
       </button>
+        <p className='text-mv font-int text-[15px] font-bold dark:text-white'>
+        {toggled ? "DARK MODE" : "LIGT MODE"}
+        </p>
+        
     </div>
+    
   );
 }
 
