3a32a1b4d5adb87faa2e5a4b2fce112877a0a535
Huseyin-Celebi <hcelebi057@gmail.com>
2024-03-12 17:53:27 +0300

    project tamamlandı

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -6,8 +6,8 @@ function Project() {
        <div className='flex-col space-y-6 md:space-y-10 lg:space-y-12'>
             <h1 className='flex text-mv dark:text-fy font-bold text-xl md:text-4xl md:leading-10 lg:text-5xl lg:leading-[58px] '>Projects</h1>
             <div className='flex flex-col md:flex-row  bg-white dark:bg-[#2B2727] md:space-x-4 lg:space-x-12 drop-shadow-lg rounded-lg'>
-                <img className="flex rounded-md md:rounded-none md:rounded-s-md lg:rounded-s-lg w-[220px] md:w-[240px] lg:w-[360px] h-[220px] md:h-[260px] lg:h-[360px]" src="src/assets/projects_1.png" alt="" />
-                <div className='grid  md:pr-6 lg:pr-10 items-center'>
+                <img className="flex rounded-md md:rounded-none md:rounded-s-md lg:rounded-s-lg w-[220px] md:w-[280px] lg:w-[360px] h-[220px] md:h-[260px] lg:h-[360px]" src="src/assets/projects_1.png" alt="" />
+                <div className='flex-col pl-1 space-y-2 md:space-y-2 lg:space-y-6 pt-4 md:pt-2 lg:pt-10 md:pr-6 lg:pr-10'>
                     <h2 className='text-mv dark:text-[#C1BAED] font-bold text-base md:text-lg lg:text-3xl'>Workintech</h2>
                     <p className='text-[#383838] dark:text-white font-normal text-xs md:text-sm lg:text-base w-[220px] md:w-[320px] lg:w-[480px]'>A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.</p>
                     <div className='space-x-2 md:space-x-5'>
@@ -16,12 +16,29 @@ function Project() {
                         <button className='text-white text-xs md:text-sm bg-mv dark:bg-Pbtn rounded-[23px] w-[48px] md:w-[68px] h-[21px] md:h-[31px] '>vercel</button>
                     </div>
                     <div className='space-x-5'>
-                        <a className="tetx-[#120B39] dark:text-fy font-medium text-[16px] underline" href="\">View Site</a>
-                        <a className="tetx-[#120B39] dark:text-fy font-medium text-[16px] underline" href="\">Github</a>
+                        <a className="tetx-[#120B39] dark:text-fy font-medium text-xs md:text-sm lg:text-[16px] underline" href="\">View Site</a>
+                        <a className="tetx-[#120B39] dark:text-fy font-medium text-xs md:text-sm lg:text-[16px] underline" href="\">Github</a>
                     </div>
+                    
                 </div>
             </div>
-            <div></div>
+            <div className='flex flex-col md:flex-row  bg-white dark:bg-[#2B2727] md:space-x-4 lg:space-x-12 drop-shadow-lg rounded-lg'>
+                <img className="flex rounded-md md:rounded-none md:rounded-s-md lg:rounded-s-lg w-[220px] md:w-[280px] lg:w-[360px] h-[220px] md:h-[280px] lg:h-[360px]" src="src/assets/projects-2.png" alt="" />
+                <div className='flex-col pl-1 space-y-2 md:space-y-2 lg:space-y-6 pt-4 md:pt-2 lg:pt-10 md:pr-6 lg:pr-10'>
+                    <h2 className='text-mv dark:text-[#C1BAED] font-bold text-base md:text-lg lg:text-3xl'>Journey</h2>
+                    <p className='text-[#383838] dark:text-white font-normal text-xs md:text-sm lg:text-base w-[220px] md:w-[320px] lg:w-[480px]'>A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.</p>
+                    <div className='space-x-2 md:space-x-5'>
+                        <button className='text-white text-xs md:text-sm bg-mv dark:bg-Pbtn rounded-[23px] w-[48px] md:w-[68px] h-[21px] md:h-[31px] '>react</button>
+                        <button className='text-white text-xs md:text-sm bg-mv dark:bg-Pbtn rounded-[23px] w-[48px] md:w-[68px] h-[21px] md:h-[31px] '>redux</button>
+                        <button className='text-white text-xs md:text-sm bg-mv dark:bg-Pbtn rounded-[23px] w-[48px] md:w-[68px] h-[21px] md:h-[31px] '>vercel</button>
+                    </div>
+                    <div className='space-x-5'>
+                        <a className="tetx-[#120B39] dark:text-fy font-medium text-xs md:text-sm lg:text-[16px] underline" href="\">View Site</a>
+                        <a className="tetx-[#120B39] dark:text-fy font-medium text-xs md:text-sm lg:text-[16px] underline" href="\">Github</a>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     </section>
   )
