eb645d67b03017f3304ff67c8e1f39de67e52e38
Huseyin-Celebi <hcelebi057@gmail.com>
2024-03-17 02:10:15 +0300

    hero2 add

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
-
-import { useState } from 'react';
-import './App.css'
-import Hero from './Components/Hero';
-import Skills from './Components/Skills';
-import Profile from './Components/Profile';
-import Project from './Components/Project';
-import Foter from './Components/Footer';
-/* import Hero2 from './Components/Hero2'; */
-
+import { useState } from "react";
+import "./App.css";
+import Hero from "./Components/Hero";
+import Skills from "./Components/Skills";
+import Profile from "./Components/Profile";
+import Project from "./Components/Project";
+import Foter from "./Components/Footer";
+/* import Hero2 from "./Components/Hero2"; */
 
 function App() {
-  
-
   return (
     <>
-    <Hero />
-   {/*  <Hero2 /> */}
-    <Skills/>
-    <Profile/>
-    <Project/>
-    <Foter/>
+      <Hero />
+      {/*  <Hero2 /> */}
+      <Skills />
+      <Profile />
+      <Project />
+      <Foter />
     </>
-  )
+  );
 }
 
 export default App;
--- src/Components/Hero2.jsx
+++ src/Components/Hero2.jsx
@@ -1,31 +1,30 @@
-import React from 'react';
-import HeroContent from '../Layout/HeroContent.jsx';
-import DarkMode from '../Layout/DarkMode.jsx';
-import { useTranslate } from '../TranslationProvider.jsx';
+import React from "react";
+import HeroContent from "../Layout/HeroContent.jsx";
+import DarkMode from "../Layout/DarkMode.jsx";
+import { useTranslate } from "../TranslationProvider.jsx";
 
 function Hero() {
-  const { toggleLanguage } = useTranslate();
+  const { language, toggleLanguage } = useTranslate();
+  console.log(language);
 
   return (
-    <main className='flex flex-col lg:flex-row w-full h-[671px]'>
-      {/* Sol sütun */}
-      <div className='flex flex-col justify-between items-center bg-mv w-full lg:w-[70%] h-full dark:bg-darkMv'>
-        <div className='relative w-[100%] lg:w-[955px] h-[72px] lg:top-[37px] lg:left-[152px] space-y-4 lg:space-y-0 lg:flex lg:items-center'> 
-          <p className='font-int font-bold text-[32px] leading-[72px] text-left text-fy'>almila</p>
-          <a onClick={toggleLanguage} className='flex text-[#777777] hover:text-slate-100 w-[338px] h-[18px] text-sm font-bold' href="/">
-            <p className='text-fy dark:text-[#BAB2E7]' onClick={() => c}>TÜRKÇE'YE GEÇ</p>
-          </a>
-        </div>
-        <HeroContent />
-      </div>
-
-      {/* Sağ sütun */}
-      <div className='flex items-center justify-center bg-fy dark:bg-darkFy w-full lg:w-[30%] h-full'>
-        <div className='relative flex gap-[16px]'>
-          <DarkMode />
+    <main className="flex bg-gradient-to-r from-mv from-70% to-fy to-30% dark:bg-gradient-to-r dark:from-darkMv dark:to-darkFy justify-center">
+      <div className="flex">
+        <h1 className="font-bold text-xl md:text-2xl lg:text-3xl text-fy">
+          huseyin
+        </h1>
+        <div className="flex gap-5 ">
+          <button
+            onClick={toggleLanguage}
+            className="text-[#777777] hover:text-slate-100 
+           text-xs md:text-xs lg:text-sm  font-bold"
+          >
+            {language === "tr" ? "CHANGE to ENGLISH" : "TÜRKÇE'YE GEÇ"}
+          </button>
+          <DarkMode className="top-0" />
         </div>
       </div>
-    </main>  
+    </main>
   );
 }
 
